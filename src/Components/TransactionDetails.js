6f3419a02fee79b7e9388dbeeed8d94a810d94f5
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 14:48:26 -0500

    adds get/delete request for index transaction

---
+++ src/Components/TransactionDetails.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+
+
+function TransactionDetails() {
+  const URL = process.env.REACT_APP_API_URL;
+  const [entry, setEntry] = useState([]);
+  let { index } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+    .get(`${URL}/transactions/${index}`)
+    .then((response) => setEntry(response.data));
+    console.log("I'm here too");
+  }, []);
+
+  const handleDelete = () => {
+    axios
+    .delete(`${URL}/transactions/${index}`)
+    .then(() => navigate("/transactions"));
+  };
+
+  return (
+    <article>
+        <p>
+            Date: {entry.date}
+        </p>
+        <p>
+            Name: {entry.name}
+        </p>
+        <p>
+            Amount: ${entry.amount}
+        </p>
+        <p>
+            Location: {entry.location}
+        </p>
+        <p>
+            Source: {entry.source}
+        </p>
+        <div>
+            <Link to={`/transactions/${index}/edit`}>
+                <button>Edit</button>
+            </Link>
+            <Link to={"/transactions"}>
+                <button>Back</button>
+            </Link>
+        </div>
+        <div>
+            {" "}
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+    </article>
+  );
+}
+
+export default TransactionDetails;
