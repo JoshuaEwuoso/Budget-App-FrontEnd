f3bd2b98935e7688ccab514184086ae96dcae561
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 14:44:21 -0500

    adds axios get request

---
+++ src/Components/TransactionApi.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Transaction from "./Transaction";
+
+
+function TransactionApi() {
+    const [entry, setEntry] = useState([]);
+    const URL = process.env.REACT_APP_API_URL;
+    
+    useEffect(() => {
+        axios
+        .get(`${URL}/transactions`)
+        .then((response) => setEntry(response.data));
+        console.log("I'm here");
+      }, []);
+
+    return (
+        <div>
+            <section>
+                <table>
+                    <thead>
+                    </thead>
+                    <tbody>
+                        {entry.map((entry, index) => {
+                            return <Transaction key={index} entry={entry} index={index} />;
+                        })}
+                    </tbody>
+                </table>
+            </section>
+        </div>
+    );
+}
+export default TransactionApi;
