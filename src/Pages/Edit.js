0d820f03b780767d0cf8e4226b0bf3948b4abb82
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 15:54:49 -0500

    adds edit page

---
+++ src/Pages/Edit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionEdit from "../Components/TransactionEdit";
+
+function Edit() {
+    return (
+            <div>
+                <h2>Edit</h2>
+                <TransactionEdit />
+            </div>
+    );
+}
+
+export default Edit;
