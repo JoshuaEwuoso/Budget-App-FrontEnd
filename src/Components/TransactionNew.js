59ac3572daa8758be5b8c3a678a420be14e9852d
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 15:26:19 -0500

    adds new transaction post request

---
+++ src/Components/TransactionNew.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+const TransactionNew = () => {
+    const URL = process.env.REACT_APP_API_URL;
+    const navigate = useNavigate();
+
+    const [newEntry, setNewEntry] = useState({
+        date: "",
+        name: "",
+        amount: "",
+        location: "",
+        source: "",
+    });
+
+    useEffect(() => {
+        axios
+        .get(`${URL}/transactions`)
+        .then((response) => setNewEntry(response.data));
+      }, []);
+
+    const handleText = (event) => {
+        setNewEntry({ ...newEntry, [event.target.id]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios
+        .post(`${URL}/transactions`, newEntry)
+        .then(() => navigate("/transactions"));
+    };
+
+    return (
+        <>
+            <div>
+                <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Entry Name:</label>
+                <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    value={newEntry.name}
+                    onChange={handleText}
+                    placeholder="Name of entry?"
+                    required
+                />
+                <label htmlFor="date">Date:</label>
+                <input
+                    id="date"
+                    name="date"
+                    type="text"
+                    value={newEntry.date}
+                    placeholder="On what date?"
+                    onChange={handleText}
+                />
+                <label htmlFor="amount">Amount:</label>
+                <textarea
+                    id="amount"
+                    name="amount"
+                    type="number"
+                    value={newEntry.amount}
+                    onChange={handleText}
+                    placeholder="How much is it?"
+                />
+                <label htmlFor="location">Location:</label>
+                <input
+                id="location"
+                name="location"
+                type="text"
+                value={newEntry.location}
+                onChange={handleText}
+                placeholder="From what account?"
+                />
+                <label htmlFor="source">Source:</label>
+                <input
+                    id="source"
+                    name="source"
+                    type="text"
+                    value={newEntry.source}
+                    placeholder="Where is it going?"
+                    onChange={handleText}
+                />
+                <br />
+                <input type="submit" />
+                </form>
+            </div>
+            <div>
+                <Link to={"/transactions"}>
+                    <button>Cancel</button>
+                </Link>
+            </div>
+        </>
+      );
+
+}
+export default TransactionNew;
