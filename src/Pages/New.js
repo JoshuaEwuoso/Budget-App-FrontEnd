d8e0646fad454ea5593401164b130ef0ae807fe4
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 15:04:54 -0500

    adds new transaction page

---
+++ src/Pages/New.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionNew from "../Components/TransactionNew";
+
+function New() {
+    return (
+            <div>
+                <h2>New Transaction</h2>
+                <TransactionNew />
+            </div>
+    );
+}
+
+export default New;
