34601243773f35a50532c03e95ac70bf85cfd4ad
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 14:19:55 -0500

    adds page for all transactions

---
+++ src/Pages/AllTransactions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionApi from "../Components/TransactionApi";
+
+function Logs() {
+    return (
+        <div>
+            <h2>All Transactions</h2>
+            <TransactionApi />
+        </div>
+    );
+}
+
+export default Logs;
