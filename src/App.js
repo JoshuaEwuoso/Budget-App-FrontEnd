449795b1711b5831ef8aefaa036a1152b5c0e754
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-20 15:55:11 -0500

    updates routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+import Edit from "./Pages/Edit";
 import Home from "./Pages/Home";
 import NavBar from "./Pages/NavBar";
+import Four0Four from "./Pages/Four0Four";
+import AllTransactions from "./Pages/AllTransactions";
 
 
 function App() {
@@ -9,6 +14,11 @@ function App() {
         <NavBar />  
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/transactions/new" element={<New/>} />
+          <Route path="/transactions" element={<AllTransactions/>} />
+          <Route path="/transactions/:index" element={<Show/>} />
+          <Route path="/transactions/:index/edit" element={<Edit />} />
+          <Route path="*" element={<Four0Four />} />
         </Routes>
       </div>
   );
