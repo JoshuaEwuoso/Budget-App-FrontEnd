574f10d3915ba096373f08613f5fca1a40a5f924
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-23 02:00:41 -0500

    adds a balance page

---
+++ src/Pages/Balance.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionBalance from "../Components/TransactionBalance";
+
+const Balance = () => {
+    return (
+        <div>
+            <h2>Balance</h2>
+            <TransactionBalance />
+        </div>
+    );
+}
+
+export default Balance;
