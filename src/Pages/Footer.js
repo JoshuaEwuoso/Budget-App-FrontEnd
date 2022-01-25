2abd0495a4e7f7cb52323d66f06fd7ec2e74d52e
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-25 16:39:55 -0500

    adds footer

---
+++ src/Pages/Footer.js
@@ -0,0 +1,13 @@
+import "../Styling/Footer.css";
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>Copyright &copy; 2022 GhostPen.</p>
+            </footer>
+        </>
+    );
+}
+
+export default Footer;
