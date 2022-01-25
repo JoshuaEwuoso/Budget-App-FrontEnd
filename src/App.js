ec489ab0079a908572e95845ccc6a93eec172ce1
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-25 11:24:47 -0500

    removes navBar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import New from "./Pages/New";
 import Show from "./Pages/Show";
 import Edit from "./Pages/Edit";
 import Home from "./Pages/Home";
-import NavBar from "./Pages/NavBar";
 import Four0Four from "./Pages/Four0Four";
 import AllTransactions from "./Pages/AllTransactions";
 import Balance from "./Pages/Balance";
@@ -12,7 +11,6 @@ import Balance from "./Pages/Balance";
 function App() {
   return (
       <div>
-        <NavBar />  
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/balances" element={<Balance/>} />
