0415871ca41014743dafcab5b030270106f5aa9e
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2022-01-25 16:43:05 -0500

    adds get request for balance

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,22 +1,34 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "../Styling/NavBar.css";
-import Balance from "../Components/TransactionBalance";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
+  const [entry, setEntry] = useState([]);
+    const URL = process.env.REACT_APP_API_URL;
+    
+    useEffect(() => {
+        axios
+        .get(`${URL}/transactions`)
+        .then((response) => setEntry(response.data));
+    }, []);
+
+    const balance = entry.map(item => Number(item.amount)).reduce((prev, curr) => Number(prev) + Number(curr), 0);
+
+    
   return (
     <nav className="NavBar">
-      <h1>
+      <h1 className="logo">
         <Link to="/" className="link">Budget App</Link>
       </h1>
       <h3>
         <Link to="/transactions" className="link all">All Transactions</Link>
       </h3>
-      <button>
-        <Link to="/transactions/new" className="link">New Transaction</Link>
-      </button>
       <h3>
-        <Link to="/balances" className="link balance">Balance: <Balance /></Link>
+        <Link to="/balances" className="link">Balance: ${balance}
+        </Link>
       </h3>
+        <Link to="/transactions/new" className="link"><button className="buttonNew">New Transaction</button></Link>
     </nav>
   );
 }
